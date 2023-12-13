7fd2a5b03ab682b9d236901765fe7acb5cc0a335
pascualmanuel <manuelnpascual@gmail.com>
2023-12-13 17:30:10 -0300

    changes

--- src/Components/Atendeme.js
+++ src/Components/Atendeme.js
@@ -121,20 +121,43 @@ const MiSeccion = () => {
       <div className="seccion-flexbox">
         <div className="columna-scrollable col-sc-1">
           <div className="qt-cont">
-            <p className="p-qt">No nos vemos pero te sigo en redes</p>
-          </div>
-          <div className="qt-cont">
             <p className="p-qt">
-              No nos vemos pero te sigo en redes Ay no hablamos nunca pero se
-              que estas bien porque te veo en redes Estas a full te veo en redes
-              <span className="name-qt">Manuel - 25 años 1</span>
+              "No nos vemos pero te sigo en redes"
+              <br />
+              <br />
+              "Ay no hablamos nunca pero se que estas bien porque te veo en
+              redes"
+              <br />
+              <br />
+              "Estas a full te veo en redes"
+              <span className="name-qt">Cynthia - 30 años</span>
             </p>
           </div>
           <div className="qt-cont">
             <p className="p-qt">
-              No nos vemos pero te sigo en redes Ay no hablamos nunca pero se
-              que estas bien porque te veo en redes Estas a full te veo en redes
-              <span className="name-qt">Manuel - 25 años 2 </span>
+              “LO MUY POCO QUE PODRÍA DECIR QUE CREO QUE ESTE IMAGINARIO
+              PERSONAJE ES QUE ES UNO DE LOS ÍNTIMOS, OCULTADOS ARQUETIPOS CON
+              QUE TODOS NOSOTROS CARGAMOS ENTRE LOS PLIEGUES DE NUESTRAS
+              SECRETAS INDIVIDUALES PSICOLOGÍAS”
+              <span className="name-qt">Blas - 85 años</span>
+            </p>
+          </div>
+          <div className="qt-cont">
+            <p className="p-qt">
+              conexión que parece, pero no es...
+              <br />
+              presencia que no es...
+              <br />
+              trama que no hay...
+              <br />
+              red que no sostiene...
+              <br />
+              alienación, indiferencia, inconsciencia, anestesia ...
+              <br />
+              no duele más...
+              <br />
+              no duele más?
+              <span className="name-qt">Eliza - 56 años</span>
             </p>
           </div>
           <div className="qt-cont">
@@ -145,46 +168,124 @@ const MiSeccion = () => {
           </div>
           <div className="qt-cont">
             <p className="p-qt">
-              No nos vemos pero te sigo en redes Ay no hablamos nunca pero se
-              que estas bien porque te veo en redes Estas a full te veo en redes
-              <span className="name-qt">Manuel - 25 años 4</span>
+              Que le salga un orzuelo al Sr Morgenstern!
+              <span className="name-qt">Pablo - 55 años</span>
             </p>
           </div>
           <div className="qt-cont">
             <p className="p-qt">
-              No nos vemos pero te sigo en redes Ay no hablamos nunca pero se
-              que estas bien porque te veo en redes Estas a full te veo en redes
-              <span className="name-qt">Manuel - 25 años 5</span>
+              En el fondo, todos somos lo mismo
+              <br />
+              <br />
+              En principio, somos todos iguales
+              <br />
+              <br />
+              Tuve una idea y no la seguí
+              <br />
+              <br />
+              Me dicen que estoy igual (que antes) pero yo no lo veo
+              <br />
+              <br />
+              Contacto visual en el ascensor
+              <br />
+              <br />
+              Los consejos no me sirven
+              <br />
+              <br />
+              Cuando me quise dar cuenta ya se habían ido
+              <br />
+              <br />
+              Completamente solo
+              <br />
+              <br />
+              Tengo muchas obligaciones/explicaciones
+              <br />
+              <br />
+              <span style={{ textDecoration: "line-through" }}>
+                Estuve pensando en lo que dije. (y no le cambiaría nada)
+              </span>
+              <br />
+              Ahora no puedo
+              <br />
+              <br />
+              Si es viejo, no me sirve.
+              <br />
+              <br />
+              <span className="name-qt">Maxi - 34 años</span>
             </p>
           </div>
           <div className="qt-cont">
             <p className="p-qt">
-              No nos vemos pero te sigo en redes
-              <span className="name-qt">Manuel - 25 años 6</span>
+              Yo, yo yo.. Para mí. A mí me pasó lo mismo. Es que cuando yo
+              estuve ahí. Ay! No viste? Yo hice uno igual en la escuela! Che,
+              ale, prestame atención, no ves que es más importante lo que me
+              pasa?! Esto es una falta de respeto a mi persona. No me traés? No
+              me llevás? No me lo regalías? Por qué nunca pensás en mí? MAMÁ!
+              MAMÁ! Mirame! Viste qué bien lo hice?! Yo soy el mejor de la
+              clase! Yo y lo sabía, iba a responder lo mismo. Lo que pasa es lo
+              siguiente, yo te explico. Son todos unos imbéciles, mejor lo hago
+              todo solo.
+              <span className="name-qt">Kiki - 26 años</span>
             </p>
           </div>
           <div className="qt-cont">
             <p className="p-qt">
-              No nos vemos pero te sigo en redes Ay no hablamos nunca pero se
-              que estas bien porque te veo en redes Estas a full te veo en redes
-              <span className="name-qt">Manuel - 25 años 7</span>
+              Prisionero de sí mismo
+              <br />
+              <br />
+              "Si me descargo me muero"
+              <br />
+              <br />
+              Muere solo o lo matan, o se mata/n?
+              <br />
+              <br />
+              Es/son vulnerable/s?
+              <br />
+              <br />
+              Tiene/n algún depredador natural?
+              <br />
+              <br />
+              <span className="name-qt">Manu - 29 años</span>
             </p>
           </div>
           <div className="qt-cont">
             <p className="p-qt">
-              No nos vemos pero te sigo en redes Ay no hablamos nunca pero se
-              que estas bien porque te veo en redes Estas a full te veo en redes
-              <span className="name-qt">Manuel - 25 años 8</span>
+              "No nos vemos pero te sigo en redes"
+              <br />
+              <br />
+              "Ay no hablamos nunca pero se que estas bien porque te veo en
+              redes"
+              <br />
+              <br />
+              "Estas a full te veo en redes"
+              <span className="name-qt">Cynthia - 30 años</span>
             </p>
           </div>
           <div className="qt-cont">
-            <p className="p-qt">No nos vemos pero te sigo en redes</p>
+            <p className="p-qt">
+              “LO MUY POCO QUE PODRÍA DECIR QUE CREO QUE ESTE IMAGINARIO
+              PERSONAJE ES QUE ES UNO DE LOS ÍNTIMOS, OCULTADOS ARQUETIPOS CON
+              QUE TODOS NOSOTROS CARGAMOS ENTRE LOS PLIEGUES DE NUESTRAS
+              SECRETAS INDIVIDUALES PSICOLOGÍAS”
+              <span className="name-qt">Blas - 85 años</span>
+            </p>
           </div>
           <div className="qt-cont">
             <p className="p-qt">
-              No nos vemos pero te sigo en redes Ay no hablamos nunca pero se
-              que estas bien porque te veo en redes Estas a full te veo en redes
-              <span className="name-qt">Manuel - 25 años 9</span>
+              conexión que parece, pero no es...
+              <br />
+              presencia que no es...
+              <br />
+              trama que no hay...
+              <br />
+              red que no sostiene...
+              <br />
+              alienación, indiferencia, inconsciencia, anestesia ...
+              <br />
+              no duele más...
+              <br />
+              no duele más?
+              <span className="name-qt">Eliza - 56 años</span>
             </p>
           </div>
           <div className="qt-cont">
